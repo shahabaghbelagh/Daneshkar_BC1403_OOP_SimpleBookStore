3e4dcd0a9e10591e3d1cdd05848aed1a90ee1ca2
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 13:28:55 +0330

    Update Book.cs
    Book class has been modified

--- Daneshkar_BC1043_OOP_SimpleBookStore/Entity/Catalog/Book.cs
+++ Daneshkar_BC1043_OOP_SimpleBookStore/Entity/Catalog/Book.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace BookStore.Models
+{
+    class Book
+    {
+        public string BookTitle { get; set; }
+
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Category BookCat{ get; set; }
+
+    }
+}
