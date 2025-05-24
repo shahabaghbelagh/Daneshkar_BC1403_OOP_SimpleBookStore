9f2a51dc51e82f4aa1d7d777567818b3206cdfca
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 13:45:08 +0330

    Update test.cs
    test class has been omitted

--- Daneshkar_BC1043_OOP_SimpleBookStore/test.cs
+++ Daneshkar_BC1043_OOP_SimpleBookStore/test.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Daneshkar_BC1403_OOP_SimpleBookStore
-{
-    internal class test
-    {
-    }
-}
