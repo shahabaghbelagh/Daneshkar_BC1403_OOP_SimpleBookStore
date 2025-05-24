ae438d252928c8cb31eebcaa540f4c983c1f0c1d
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 14:28:16 +0330

    Add files via upload

---
+++ Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Models;
+
+namespace BookStore.Models
+{
+    class Order : IDiscount
+    {
+        public Customer Ourcustomer { get; set; }
+
+        public void AddBook(Book book)
+        {
+            Ourcustomer.bookroders.Add(book);
+        }
+
+        public decimal ApplyDiscount(decimal amount)
+        {
+            decimal z = amount;
+            return z;
+        }
+
+        public decimal CalculateTotal()
+        {
+            decimal x= 10;
+            
+            return x;
+        }
+
+    }
+}
