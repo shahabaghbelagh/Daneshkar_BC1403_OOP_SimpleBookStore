09e36a4b1ffd953013de5725040fca2147b552a9
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 14:27:35 +0330

    Add files via upload

---
+++ FixedAmountDiscount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Models
+{
+    class FixedAmountDiscount : IDiscount
+    {
+        public decimal dol { get; set; }
+
+        public FixedAmountDiscount(decimal doll)
+        {
+            dol = doll;
+        }
+
+        public decimal ApplyDiscount(decimal amount)
+        {
+
+            decimal finalPrice = amount;
+            return finalPrice;
+        }
+    }
+}
