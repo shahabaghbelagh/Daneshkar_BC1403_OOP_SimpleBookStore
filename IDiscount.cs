2e73f5d2a534b1c5cee4d9908a9b6c1f6e0c0dcc
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 14:27:53 +0330

    Add files via upload

---
+++ IDiscount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Models
+{
+    interface IDiscount
+    {
+        decimal ApplyDiscount(decimal amount);
+    }
+}
