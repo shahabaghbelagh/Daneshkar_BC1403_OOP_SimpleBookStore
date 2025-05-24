8511a2a68786ab7e255286a08446e570f5cdc915
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 14:26:57 +0330

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    class Category
+    {
+        public string BookCategory { get; set; }
+
+        public Category(string cat)
+        {
+            BookCategory = cat;
+        }
+    }
+}
