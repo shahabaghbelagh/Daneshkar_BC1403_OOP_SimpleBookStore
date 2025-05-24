3d7db85e516a88942bfb89b9c7b1a69e23cc2492
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 14:26:37 +0330

    Add files via upload
    Book class has been changed

---
+++ Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    class Book
+    {
+        public string BookTitle { get; set; }
+
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Category BookCat{ get; set; }
+
+
+        public Book(string title, string author, decimal price, Category cat)
+        {
+            BookTitle = title;
+
+            Author = author;
+
+            Price = price;
+
+            BookCat = cat;
+        }
+
+    }
+}
