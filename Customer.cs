5256053472107aec14159c08ad0b419087d8dcdd
shahabaghbelagh <137681182+shahabaghbelagh@users.noreply.github.com>
2025-05-24 14:27:15 +0330

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    class Customer
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Book> bookroders { get; set; }
+
+        public Customer(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+    }
+}
